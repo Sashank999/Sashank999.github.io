903e7f7f7a14e36c16c9194d5a854a307278ea5d
Sashank999 <bandi.rao999@gmail.com>
2025-11-22 16:31:01 +0530

    Add quotes database processing script to publishing workflow.

---
+++ portfolio_quotes_processor.js
@@ -0,0 +1,43 @@
+import fs from "node:fs";
+
+if (process.argv.length != 4) throw Error("Incorrect arguments.");
+
+const inputFilePath = process.argv[2];
+const outputFilePath = process.argv[3];
+
+const inputStream = fs.createReadStream(inputFilePath, {
+  encoding: "utf8",
+  highWaterMark: 1024 * 1024,
+});
+const outputStream = fs.createWriteStream(outputFilePath);
+
+const ONE_BYTE = 2 ** 8;
+let currentQuote = "",
+  quoteIndex = 0;
+for await (const line of inputStream) {
+  for (const char of line) {
+    if (char == "\n") {
+      const quoteLength = currentQuote.length;
+      quoteIndex += quoteLength;
+
+      let quoteIndexParts = [];
+      while (quoteIndex > 0) {
+        quoteIndexParts.unshift(quoteIndex % ONE_BYTE);
+        quoteIndex = Math.floor(quoteIndex / ONE_BYTE);
+      }
+
+      while (quoteIndexParts.length < 3) {
+        quoteIndexParts.unshift(0);
+      }
+
+      if (quoteIndexParts.length > 3)
+        throw Error(`Quote index ${quoteIndex} exceeded 2 ** 24.`);
+
+      outputStream.write(new Uint8Array(quoteIndexParts));
+
+      currentQuote = "";
+    } else {
+      currentQuote += char;
+    }
+  }
+}
